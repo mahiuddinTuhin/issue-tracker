25dadca18f992509bcc37e3fed72bb725bfc0e91
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2023-11-07 04:10:28 +0600

    styling the active link in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,21 +1,32 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
-import { AiFillBug } from "react-icons/ai";
+import { PiBugDuotone } from "react-icons/pi";
 function Navbar() {
+  const currentPathname = usePathname();
+
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
-
+  console.log(currentPathname);
   return (
     <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
       <Link href="/">
-        <AiFillBug />
+        <PiBugDuotone className="text-3xl" />
       </Link>
-      <ul className="flex space-x-6 text-zinc-500">
+      <ul className="flex space-x-6 text-zinc-500 text-2xl font-semibold">
         {links?.map((link, i) => {
           return (
-            <li key="i" className="hover:text-zinc-900">
+            <li
+              key="i"
+              className={classNames({
+                "hover:text-zinc-500 transition-colors": true,
+                "text-zinc-900": link?.href === currentPathname,
+              })}
+            >
               <Link href={link?.href}>{link?.label}</Link>
             </li>
           );
